f58ec92453eac49baf3abd773d86fc446d354280
Shane Coufreur <shane.coufreur@gmail.com>
2022-12-03 15:02:24 +0100

    Add day 3 solution

---
+++ 3/code.py
@@ -0,0 +1,39 @@
+# Advent of code Year 2022 Day 3 solution
+# Author = Shane Coufreur
+# Date = December 2022
+
+import string
+
+with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+    input = [l.strip() for l in input_file.readlines()]
+
+priorities = list(" ")
+priorities += list(string.ascii_lowercase) + list(string.ascii_uppercase)
+
+#print(priorities)
+
+def solve1():
+    matches = []
+    for l in input:
+        p1, p2 = l[:len(l)//2], l[len(l)//2:]
+
+        for c in p1:
+            if c in p2:
+                matches.append(c)
+                break
+
+    return sum([priorities.index(m) for m in matches])
+    
+def solve2():
+    matches = []
+    grouplength = 3
+    for g in range(len(input)//grouplength):
+        for c in input[0+g*grouplength]:
+            if c in input[1+g*grouplength] and c in input[2+g*grouplength]:
+                matches.append(c)
+                break
+            
+    return sum([priorities.index(m) for m in matches])
+
+print("Part One : "+ str(solve1()))
+print("Part Two : "+ str(solve2()))
