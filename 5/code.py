e65c01a8e2aa50173c948fa7f5ad15a4e2e5e722
Shane Coufreur <shane.coufreur@gmail.com>
2022-12-05 14:30:20 +0100

    Refactor and add large input

--- 5/code.py
+++ 5/code.py
@@ -1,9 +1,23 @@
 # Advent of code Year 2022 Day 5 solution
 # Author = Shane Coufreur
 # Date = December 2022
+from time import time
+  
+def timer(func):
+    # This function shows the execution time of 
+    # the function object passed
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
+        return result
+    return wrap_func  
+
 
 containers = []
 instructions = []
+
 def parse():
     global containers, instructions
     stacks, instructions = input.split("\n\n")
@@ -19,35 +33,55 @@ def parse():
         containers.append(column)
     return containers
 
+@timer
 def solve1():
     parse()
     global instructions, containers
-    for instruction in instructions:
+    for i, instruction in enumerate(instructions):
         _, n, _, f, _, t = instruction.split(" ")
-
-        for _ in range(int(n)):
-            tmp, containers[int(f)-1] = containers[int(f)-1][0], containers[int(f)-1][1:]
-            containers[int(t)-1] = [tmp] + containers[int(t)-1]
+        if i % (len(instructions) / 10000) == 0:
+            pass
+            #print( f"Handling {i} of {len(instructions)} instructions.")
+        tmp, containers[int(f)-1] = containers[int(f)-1][:int(n)], containers[int(f)-1][int(n):]
+        tmp.reverse()
+        tmp += containers[int(t)-1]
+        containers[int(t)-1] = tmp
     
     return "".join([c[0] for c in containers])
 
+@timer
 def solve2():
     parse()
     global instructions, containers
-    for instruction in instructions:
+    for i, instruction in enumerate(instructions):
         _, n, _, f, _, t = instruction.split(" ")
+        if i % (len(instructions) / 10000) == 0:
+            pass
+            #print( f"Handling {i} of {len(instructions)} instructions.")
 
         tmp, containers[int(f)-1] = containers[int(f)-1][:int(n)], containers[int(f)-1][int(n):]
-        containers[int(t)-1] = tmp + containers[int(t)-1]
+        #containers[int(t)-1] = tmp + containers[int(t)-1]
+        tmp += containers[int(t)-1]
+        containers[int(t)-1] = tmp
     
     return "".join([c[0] for c in containers])
 
-with open((__file__.rstrip("code.py")+"testinput.txt"), 'r') as input_file:
-    input = input_file.read()
-print("Part One : "+ str(solve1()))
-print("Part Two : "+ str(solve2()))
+# with open((__file__.rstrip("code.py")+"testinput.txt"), 'r') as input_file:
+#     input = input_file.read()
+# print("Part One : "+ str(solve1()))
+# print("Part Two : "+ str(solve2()))
 
-with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+# with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+#    input = input_file.read()
+# print("Part One : "+ str(solve1()))
+# print("Part Two : "+ str(solve2()))
+
+# with open((__file__.rstrip("code.py")+"largeinput.txt"), 'r') as input_file:
+#    input = input_file.read()
+# print("Part One : "+ str(solve1()))
+# print("Part Two : "+ str(solve2()))
+
+with open((__file__.rstrip("code.py")+"hugeinput.txt"), 'r') as input_file:
    input = input_file.read()
 print("Part One : "+ str(solve1()))
 print("Part Two : "+ str(solve2()))
